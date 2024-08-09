99e25a1e90375243158967086da8ac717dc5407a
TAIWO KOLAWOLE <omokolataiwo@gmail.com>
2024-08-09 12:33:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const list = [];
 const fn = () => {
     let items = [];
 
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     res.send(list);
 });
 
-const port = 3090;
+const port = 8080;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
     fn();
